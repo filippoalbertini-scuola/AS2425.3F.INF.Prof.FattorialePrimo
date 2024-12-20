69b0f85824b1f56787a3dc8f9e8f45a46bb92d5e
Filippo Albertini <filippo@filippoalbertini.it>
2024-12-20 21:26:01 +0100

    feat: menù and dummy function creation

--- AS2425.3F.INF.Prof.FattorialePrimo/Program.cs
+++ AS2425.3F.INF.Prof.FattorialePrimo/Program.cs
@@ -4,7 +4,85 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Prof;3F;21/12/24;Calcolo del fattoriale e verifica se un numero è primo");
+
+            int scelta = 0;
+            int numero = 0;
+            int risultato = 0;
+
+            do
+            {
+                scelta = Menu();
+
+                switch (scelta)
+                {
+                    case 1: // Fattoriale di un numero
+
+                        Console.WriteLine("Inserisci il numero su cui calcolare il fattoriale");
+
+                        numero = Convert.ToInt32(Console.ReadLine());
+
+                        risultato = Fattoriale(numero);
+
+                        Console.WriteLine($"Il fattoriale di {numero} è {risultato}");
+                        
+                        break;
+
+                    case 2: // Dato un numero indicarne la radice quadrata
+
+                        Console.WriteLine("Inserisci il numero da verificare se è primo");
+                        numero = Convert.ToInt32(Console.ReadLine());
+
+                        if (IsPrimo(numero))
+                            Console.WriteLine($"Il numero {numero} è primo");
+                        else
+                            Console.WriteLine($"Il numero {numero} non è primo");
+
+                        break;
+                    case 9:
+                        Console.WriteLine("Hai scelto di uscire.");
+                        break;
+                }
+
+            } while (scelta != 9);
+
+            Console.WriteLine("Programma terminato.");
+        }
+
+        /// <summary>
+        /// Indica all'utente le possibili scelte
+        /// </summary>
+        static int Menu()
+        {
+            int scelta = 0;
+
+            Console.WriteLine("Scelte operative :");
+            Console.WriteLine("1) Calcola il fattoriale di numero");
+            Console.WriteLine("2) Verifica se numero è primo");
+            Console.WriteLine("9) Uscita");
+
+            // TODO:  controlla i valori indicati dall'utente
+            scelta = Convert.ToInt32(Console.ReadLine());
+
+            return scelta;
+        }
+
+        /// <summary>
+        /// Calcola il fattoriale di numero
+        /// </summary>
+        /// <returns></returns>
+        static int Fattoriale(int numero)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Verifica se numero è primo
+        /// </summary>
+        /// <returns>True se primo</returns>
+        static bool IsPrimo(int numero)
+        {
+            return false;
         }
     }
 }
